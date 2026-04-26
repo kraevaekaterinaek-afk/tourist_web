953b2ea77d9d28d5b25a509f22efb535d3fc98ea
Соня <listostryj@gmail.com>
2026-04-26 14:12:14 +0500

    add auth

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from peewee import *
 
 app = Flask(__name__)
+app.secret_key = ",sf';lk,4[po6i2046-0lx,ztoit0-490=084630-2d"
+
 db = SqliteDatabase('tourist.db')
+
 
 class Users(Model):
     nickname = CharField(16)
     password = CharField()
-
 
     class Meta:
         database = db
@@ -46,9 +49,74 @@ class Achievement(Model):
 
 db.create_tables([Users, Diary, Budget, Achievement])
 
-@app.route("/")
+
+@app.route('/')
 def index_page():
-    return render_template('index.html')
+    if session.get("nickname"):
+        return render_template('index.html')
+    return redirect('/auth')
+
+@app.route("/registration")
+def registration_page():
+    if session.get("nickname"):
+        return redirect("/")
+    return render_template("registration.html")
+
+@app.route("/registration", methods=["POST"])
+def registration():
+    nickname = request.form["nickname"]
+    password = request.form["password"]
+    password_r = request.form["password_r"]
+
+    if password != password_r:
+        flash("Пароли не совпадают")
+        return redirect("/registration")
+    
+    if Users.select().where(Users.nickname == nickname).exists():
+        flash("Такой логин уже существует")
+        return redirect("/registration")
+    
+    try:
+        password = generate_password_hash(password)
+        user = Users.create(
+            nickname=nickname,
+            password=password,
+        )
+        session["nickname"] = user.nickname
+        return redirect("/")
+    except:
+        flash("Ошибка сервера")
+        return redirect("/registration")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route("/auth")
+def auth_page():
+    if session.get("nickname"):
+        return redirect("/")
+    return render_template("auth.html")
+
+@app.route("/auth", methods=["POST"])
+def authentication():
+    nickname = request.form["nickname"]
+    password = request.form["password"]
+
+    try:
+        user = Users.get(Users.nickname == nickname)
+    except:
+        flash("Такой никнейм не зарегистрирован")
+        return redirect("/auth")
+    
+    if check_password_hash(user.password, password):
+        session["nickname"] = user.nickname
+        return redirect("/")
+    else:
+        flash("Неверный пароль")
+        return redirect("/auth")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
